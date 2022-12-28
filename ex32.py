a9ed76bf58db17f2dabfc0df8a65a8c2abe479e3
yaldachamani <120212661+yaldachamani@users.noreply.github.com>
2022-12-28 00:48:20 -0800

    Delete ex32.py

--- ex32.py
+++
@@ -1,6 +0,0 @@
-n = int(input("please insert the rows : "))
-m = int(input("please insert the columns : "))
-array = []
-array = [["#*" for j in range(m)] for i in range(n)]
-for i in range(n):
-    print(array[i])
